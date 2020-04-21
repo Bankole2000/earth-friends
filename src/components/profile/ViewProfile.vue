98d4c854aff12baa7d778a77e0ed6e75ad838cc2
Bankole Esan <techybanky@gmail.com>
2020-04-21 04:20:13 +0100

    first push

--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -3,6 +3,16 @@
     <div class="view-profile container">
       <div v-if="profile" class="card">
         <h2 class="deep-purple-text center">{{ profile.alias }}'s Wall</h2>
+        <ul class="comments collection">
+          <li>Comment</li>
+        </ul>
+        <form @submit.prevent='addComment'>
+          <div class="field">
+            <label for="comment">Add a comment</label>
+            <input type="text" name="comment" v-model="newComment">
+            <p v-if="feedback">{{ feedback }}</p>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -16,6 +26,8 @@ export default {
   data() {
     return {
       profile: null,
+      newComment: null,
+      feedback: null,
     };
   },
   created() {
